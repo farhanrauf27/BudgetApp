1c96bb9285a595e48bb7d582b9b0466fbbd3a186
farhanrauf27 <farhanrauf27@gmail.com>
2025-12-23 18:44:49 +0500

    API updated

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import { Transaction, MonthlySummary } from '../types';
 
-// const API_URL = 'https://budgetbackend-shoa.onrender.com/api';
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://budgetbackend-shoa.onrender.com/api';
+// const API_URL = 'http://localhost:5000/api';
 
 const api = axios.create({
   baseURL: API_URL,
